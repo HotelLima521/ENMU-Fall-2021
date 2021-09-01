b2c42e26bbe3b916e8281e52fc91745610cb5d88
Derek White <white.derek26@pm.me>
2021-09-01 14:43:28 -0600

    Added notes from 241/122

---
+++ EET/122 - Programming Fundamentals with C & C++/pow_sqrt2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+// Convert the following math expression to C++ expression: (1 + r/100)^n
+
+	int n;
+	int r;
+	int x = 1+r/100;
+
+	answer = pow(x, n);
+	
+// Convert the math expression to c++ expression sqrt(a^2+b^2)
+
+
+	int a;
+	int b;
+
+	answer = sqrt(a^2+b^2); 
+}
